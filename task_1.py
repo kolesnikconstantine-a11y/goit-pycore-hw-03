90243d76f1a847594d1abab379b6fe98a540336d
Kolesnik Constantine <kolesnikconstantine@gmail.com>
2026-04-27 14:58:23 +0300

    Changed some code

--- task_1.py
+++ task_1.py
@@ -4,19 +4,22 @@ import datetime
 # Функція приймає один параметр: date — рядок, що представляє дату у форматі 'РРРР-ММ-ДД'
 def get_days_from_today(date):
 
-# Перетворення рядка в об'єкт datetime
-    datetime_object = datetime.datetime.strptime(date, "%Y-%m-%d")
-    
-# Отримання поточної дати, використовуючи datetime.today().
-    now = datetime.datetime.today()
+    try:
+        # Перетворення рядка в об'єкт datetime
+        datetime_object = datetime.datetime.strptime(date, "%Y-%m-%d")
+   
+        # Отримання поточної дати, використовуючи datetime.today().
+        now = datetime.datetime.today()
 
-# Розрахунок різниці між поточною датою та заданою датою.
-    days_diff = now - datetime_object
-    print(days_diff.days)
+        # Розрахунок різниці між поточною датою та заданою датою.
+        days_diff = now - datetime_object
+        print(days_diff.days)
+
+    # Обробка винятка, якщо неправильний формат вхідних даних
+    except ValueError:
+        print(f"Date {date} does not match format '%Y-%m-%d")
 
 # Функція повертає ціле число, яке вказує на кількість днів від заданої дати до поточної.
-get_days_from_today("2021-10-01")
+get_days_from_today("2028-12-01")
 
 
-#Обробка винятків: функція має впоратися з неправильним форматом вхідних даних.
-#?????? как обработать 

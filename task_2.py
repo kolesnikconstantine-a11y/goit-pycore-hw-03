96d8b744b5b495cb2c5f46a39ee5352138f2bd71
Kolesnik Constantine <kolesnikconstantine@gmail.com>
2026-04-30 10:47:05 +0300

    Changes to function

--- task_2.py
+++ task_2.py
@@ -3,16 +3,17 @@ import random
 
 def get_numbers_ticket(min, max, quantity):
     # Перевірка заданих параметрів
-    if min >= 1 and max <= 1000 and quantity < max and quantity > min:
+    if min >= 1 and max <= 1000:
         # Формування списка numbers, з урахування min та max 
-        numbers = []
-        index = min
-        while index <= max:
-            numbers.append(index)
-            index +=1
+        #numbers = []
+        #index = min
+        #while index <= max:
+        #    numbers.append(index)
+        #    index +=1
+        
     
         # Використання методу sample(), який повертає список довжиною quantity з унікальними елементами, вибраними випадково з numbers.
-        chosen_numbers = random.sample(numbers, quantity)
+        chosen_numbers = random.sample(range(min, max + 1), quantity)
         # Використання методу sorted(), який повертає новий відсортований об'єкт
         sorted_num = sorted(chosen_numbers)
         # Функиція повертає список випадково вибраних, відсортованих чисел. 
@@ -23,7 +24,7 @@ def get_numbers_ticket(min, max, quantity):
 
 
 
-lottery_numbers = get_numbers_ticket(10, 49, 12)
+lottery_numbers = get_numbers_ticket(10, 49, 5)
 print("Ваші лотерейні числа:", lottery_numbers)
 
 
